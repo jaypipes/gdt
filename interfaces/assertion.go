666846751e383b01e1dadcfc33b58a650cf159b1
Jay Pipes <jaypipes@gmail.com>
2019-08-24 12:51:09 -0400

    remove now-unused interfaces/assertion.go

--- interfaces/assertion.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-// Assertion is used by the test framework to generalize a thing that asserts
-// some truth value
-type Assertion interface {
-	// Assert returns whether the assertion succeeded and a string indicating
-	// why the assertion did not succeed
-	Assert() (bool, string)
-}
