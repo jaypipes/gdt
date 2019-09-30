050f9ce4661fd56c668e46e371eeac3e955f885d
Jay Pipes <jaypipes@gmail.com>
2019-09-30 12:46:58 -0400

    update dependencies and wrap ErrInvalid
    Adds github.com/pkg/errors as dependency when wrapping
    http.ErrInvalidXXX with gdt.ErrInvalid. While adding this dependency,
    updated the other dependencies.

--- errors.go
+++ errors.go
@@ -3,9 +3,6 @@ package gdt
 import "errors"
 
 var (
-	// ErrNilTestcase is returned when attempting to call a Testcase method
-	// with a nil Testcase pointer
-	ErrNilTestcase = errors.New("nil test file")
 	// ErrUnknownParser is returned when a testcase type name string isn't
 	// found in the list of registered test case parsers
 	ErrUnknownParser = errors.New("unknown parser")
--- http/errors.go
+++ http/errors.go
@@ -1,11 +1,17 @@
 package http
 
-import "errors"
+import (
+	"github.com/pkg/errors"
+
+	"github.com/jaypipes/gdt"
+)
 
 var (
 	// ErrInvalidAliasOrURL is returned when the test author failed to provide
 	// either a URL and Method or specify one of the aliases like GET, POST, or
 	// DELETE
-	// TODO(jaypipes): wrap with gdt.ErrInvalid
-	ErrInvalidAliasOrURL = errors.New("Either specify a URL and Method or specify one of GET, POST, PUT, PATCH or DELETE")
+	ErrInvalidAliasOrURL = errors.Wrap(
+		gdt.ErrInvalid,
+		"Either specify a URL and Method or specify one of GET, POST, PUT, PATCH or DELETE",
+	)
 )
