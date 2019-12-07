3be3396662664cffc38c16d3d35e005dac284388
Jay Pipes <jaypipes@gmail.com>
2019-12-07 13:56:22 -0500

    Add Context.Basedir field
    We will need a base filepath from which to look up JSONSchema reference
    docs, so add it to the gdt.Context.

--- context.go
+++ context.go
@@ -3,7 +3,9 @@ package gdt
 // Context contains fixtures and other state that is passed to the Parse
 // function
 type Context struct {
-	// FixtureRegistry is a pointer to the fixture registry used by the test
-	// files
+	// Fixtures is a pointer to the fixture registry used by the test files
 	Fixtures FixtureRegistry
+	// Basedir is an absolute filepath to the directory that contains the test
+	// files
+	Basedir string
 }
